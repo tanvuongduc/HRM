baad0df83b688f07100f3e93fa06bbef738b1a4e
Triều Lê <ltrungtrieu@gmail.com>
2021-04-20 10:16:05 +0700

    update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { CompanyController } from './company/company.controller';
+import { TeamController } from './team/team.controller';
+import { TeamService } from './team/team.service';
+import { TeamModule } from './team/team.module';
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, TeamModule],
+  controllers: [AppController, CompanyController, TeamController],
+  providers: [AppService, TeamService],
 })
 export class AppModule {}
