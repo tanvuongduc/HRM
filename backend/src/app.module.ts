ae858b1255d813826b41fc8eaa2a78a33d736e65
Triều Lê <ltrungtrieu@gmail.com>
2021-04-20 16:28:28 +0700

    tét tý \

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,10 +6,14 @@ import { CompanyController } from './company/company.controller';
 import { TeamController } from './team/team.controller';
 import { TeamService } from './team/team.service';
 import { TeamModule } from './team/team.module';
+import { CompanyService } from './company/company.service';
+import { CompanyModule } from './company/company.module';
+import { MongooseModule } from '@nestjs/mongoose'
+
 
 @Module({
-  imports: [UserModule, TeamModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false'),UserModule, TeamModule, CompanyModule],
   controllers: [AppController, CompanyController, TeamController],
-  providers: [AppService, TeamService],
+  providers: [AppService, TeamService, CompanyService],
 })
-export class AppModule {}
+export class AppModule { }
--- backend/src/team/team.controller.ts
+++ backend/src/team/team.controller.ts
@@ -1,6 +1,4 @@
 import { Controller } from '@nestjs/common';
 
 @Controller('team')
-export class TeamController {
-    
-}
+export class TeamController {}
