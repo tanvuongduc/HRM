edf70049b493931f94c100be2181db9ee3395b11
ThaiHien170294 <thaihien17294@gmail.com>
2021-04-22 11:28:35 +0700

    api company

--- backend/src/company/company.controller.ts
+++ backend/src/company/company.controller.ts
@@ -1,19 +1,19 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { CompanyService } from './company.service';
 
 @Controller('company')
 export class CompanyController {
-    constructor ( private readonly companyServive: CompanyService){}
-     @Post()
-     async addCompany(
-         @Body('name') name: string,
-         @Body('domain') domain: string,
-         @Body('overview') overview: string,
-         @Body('address') address: string,
-         @Body('contact') contact: string,
-         @Body('pic') pic: string,
-         @Body('notes') notes: string,
-     ){
+    constructor(private readonly companyServive: CompanyService) { }
+    @Post()
+    async addCompany(
+        @Body('name') name: string,
+        @Body('domain') domain: string,
+        @Body('overview') overview: string,
+        @Body('address') address: string,
+        @Body('contact') contact: string,
+        @Body('pic') pic: string,
+        @Body('notes') notes: string,
+    ) {
         const generatedId = await this.companyServive.insertCompany(
             name,
             domain,
@@ -23,12 +23,46 @@ export class CompanyController {
             pic,
             notes
         )
-        return { id: generatedId}
-     }
+        return { id: generatedId }
+    }
 
     @Get()
-     async getAllCompany(){
+    async getAllCompany() {
         const companys = await this.companyServive.getCompany();
-        return  companys
+        return companys
+    }
+
+    @Get(':id')
+    async getSingleCompany(
+        @Param('id') id: string
+    ) {
+        return this.companyServive.getSingleCompany(id)
+    }
+    @Put(':id')
+    async update(
+        @Param('id') id: string,
+        @Body('name') name: string,
+        @Body('domain') domain: string,
+        @Body('overview') overview: string,
+        @Body('address') address: string,
+        @Body('contact') contact: string,
+        @Body('pic') pic: string,
+        @Body('notes') notes: string,
+    ) {
+        await this.companyServive.updateCompany(
+            id,
+            name,
+            domain,
+            overview,
+            address,
+            contact,
+            pic,
+            notes)
+        return { success: true }
+    }
+    @Delete(':id')
+    async removeCompany(@Param('id') id: string){
+        await this.companyServive.deleteCompany(id)
+        return{ success: true}
     }
 }
--- backend/src/company/company.service.ts
+++ backend/src/company/company.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -9,6 +9,7 @@ export class CompanyService {
     constructor(
         @InjectModel('Company') private readonly companyModel: Model<Company>,
     ) { }
+    //post insert
     async insertCompany(
         name: string,
         domain: string,
@@ -30,18 +31,88 @@ export class CompanyService {
         const result = await newCompany.save();
         return result.id as string;
     }
+    //get all
     async getCompany() {
         const companys = await this.companyModel.find().exec();
         return companys.map(prod => ({
-          id: prod.id,
-          name: prod.name,
-          domain: prod.domain,
-          overview: prod.overview,
-          address: prod.address,
-          contact: prod.contact,
-          pic: prod.pic,
-          notes: prod.notes,
-         
+            id: prod.id,
+            name: prod.name,
+            domain: prod.domain,
+            overview: prod.overview,
+            address: prod.address,
+            contact: prod.contact,
+            pic: prod.pic,
+            notes: prod.notes,
+
         }));
-      }
+    }
+    //get single
+    async getSingleCompany(id: string) {
+        const companies = await this.findCompany(id)
+        return {
+            id: companies.id,
+            name: companies.name,
+            domain: companies.domain,
+            overview: companies.overview,
+            address: companies.address,
+            contact: companies.contact,
+            pic: companies.pic,
+            notes: companies.notes,
+        }
+    }
+    //update Company
+    async updateCompany(
+        id: string,
+        name: string,
+        domain: string,
+        overview: string,
+        address: string,
+        contact: string,
+        pic: string,
+        notes: string,
+    ) {
+        const updateCompany = await this.findCompany(id);
+        if (name) {
+            updateCompany.name = name;
+        }
+        if (domain) {
+            updateCompany.domain = domain;
+        }
+        if (overview) {
+            updateCompany.overview = overview;
+        }
+        if (address) {
+            updateCompany.address = address;
+        }
+        if (contact) {
+            updateCompany.contact = contact;
+        }
+        if (pic) {
+            updateCompany.pic = pic;
+        }
+        if (notes) {
+            updateCompany.notes = notes;
+        }
+        updateCompany.save();
+    }
+    //delete company
+    async deleteCompany(id: string) {
+        const result = await this.companyModel.deleteOne({_id: id}).exec();
+        if (result.n === 0) {
+          throw new NotFoundException('Could not find Company.');
+        } 
+    }
+
+    private async findCompany(id: string): Promise<Company> {
+        let company;
+        try {
+            company = await this.companyModel.findById(id).exec();
+        } catch (error) {
+            throw new NotFoundException('Could not find Company.');
+        }
+        if (!company) {
+            throw new NotFoundException('Could not find Company.');
+        }
+        return company;
+    }
 }
