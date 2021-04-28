075962556b1c276e467b07d2af5e00ff8fb38a86
7un7un <dongvanhongpink@gmail.com>
2021-04-28 17:49:56 +0700

    ok

--- frontend/src/Modules/Components/Career/ItemTrophy.js
+++ frontend/src/Modules/Components/Career/ItemTrophy.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import trophy_01 from '../../../Asset/Img/trophy_01.svg';
-import trophy_02 from '../../../Asset/Img/trophy_02.svg';
+import trophy from '../../../Asset/Img/trophy.svg';
+import trophy2 from '../../../Asset/Img/trophy2.svg';
 
 class ItemTrophy extends Component {
     render() {
@@ -8,11 +8,11 @@ class ItemTrophy extends Component {
             <div className='item-content-right'>
                 <h2>My Career</h2>
                 <p>
-                    <img src={trophy_01} alt={trophy_01} />&ensp;
+                    <img src={trophy} alt={trophy} />&ensp;
                     Hội nghị nhất trí với 5 điểm lớn theo đề nghị của Nguyễn Ái Quốc và quyết định hợp nhất các tổ chức cộng sản, lấy tên là Đảng Cộng sản Việt Nam.
                 </p>
                 <p>
-                    <img src={trophy_02} alt={trophy_02} />&ensp;
+                    <img src={trophy2} alt={trophy2} />&ensp;
                     Hội nghị thảo luận và thông qua các văn kiện: Chánh cương vắn tắt, Sách lược vắn tắt, Chương trình tóm tắt và Điều lệ vắn tắt của Đảng Cộng sản Việt Nam.
                 </p>
             </div>
--- frontend/src/Modules/Components/Department/ItemDepartment.js
+++ frontend/src/Modules/Components/Department/ItemDepartment.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import avt from '../../../Asset/Img/avatar.png';
+import avt2 from '../../../Asset/Img/avatar2.png';
 
 class ItemDepartment extends Component {
     render() {
@@ -11,7 +13,19 @@ class ItemDepartment extends Component {
                     <div className='item-content-top'>
                         <b>Department</b>
                     </div>
-                    <div className='item-content-bottom'></div>
+                    <div className='item-content-bottom'>
+                        <p>MEMBER</p>
+                        <div className='content-member'>
+                            <div className='content-member-avt'>
+                                <img src={avt} alt={avt} />
+                                <img src={avt2} alt={avt2} />
+                            </div>
+                            <div className='content-member-nav'>
+                                <button>&#8690;</button>
+                                {/* &#8689; */}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
