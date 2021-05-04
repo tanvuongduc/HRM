f6ffd606939622dcaf684c94adba810ae06fead9
ThaiHien170294 <thaihien17294@gmail.com>
2021-05-05 01:13:54 +0700

    career

--- frontend/src/Modules/Company/Components/Career/CareerEdit.js
+++ frontend/src/Modules/Company/Components/Career/CareerEdit.js
@@ -1,17 +1,32 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import avatar from '../../../../Asset/Img/avatar.png';
+
 
 class CareerEdit extends Component {
     render() {
         return (
-            <>
-                career
-            </>
+            <div className='career-item-content'>
+                <div className='item-content-left'>
+                    <Card className='card-content'>
+                        <CardImg top src={avatar} alt={avatar} className='card-img' />
+                        <CardBody>
+                            <CardTitle tag="h5">MR: {localStorage.getItem('user')}</CardTitle>
+                            <CardSubtitle tag="h6" className="mb-2 text-muted">Manager</CardSubtitle>
+                            <CardText>Join date: 01/01/2020</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+                <div className='item-content-right'>
+                    <h2>My Career</h2>
+                    <p> Hội nghị nhất trí với 5 điểm lớn theo đề nghị của Nguyễn Ái Quốc và quyết</p>
+                    <p> Hội nghị thảo luận và thông qua các văn kiện: Chánh cương vắn</p>
+                </div>
+
+            </div>
+
         );
     }
 }
-
-CareerEdit.propTypes = {
-
-};
 
 export default CareerEdit;
