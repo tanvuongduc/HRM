eb6b20a42a6c40c82a6e47061ee0407e316deb9a
unknown <nghiemthodo1998@gmail.com>
2021-05-04 15:55:51 +0700

    update navbar

--- frontend/src/Modules/Org/Components/People/MyTeam/MyTeam.js
+++ frontend/src/Modules/Org/Components/People/MyTeam/MyTeam.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import Team from './Team/Team';
 
 class MyTeam extends Component {
     render() {
         return (
-            <div>
-                ád
+            <div className="list-team">
+                <div className="add-team">
+                    <h3 className="list-my-team"><b>List team</b></h3>
+                    <button className="btn-addNewTeam"><b>Tạo team mới</b></button>
+                </div>
+                <Team/>
             </div>
         );
     }
 }
 
-export default MyTeam;
+export default withRouter(MyTeam);
--- frontend/src/Modules/Org/Components/People/People.js
+++ frontend/src/Modules/Org/Components/People/People.js
@@ -1,7 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import { withRouter } from 'react-router';
 import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
-import AllTeam from './AllTeam/AllTeam';
 import MyTeam from './MyTeam/MyTeam';
 import User from './User/User';
 
@@ -12,18 +11,18 @@ class People extends Component {
                 <Fragment>
                     <div className="card-people">
                         <div className="list">
-                            <h1 className="card-name"><b>People</b></h1>
-                            <ul className="list-people">
-                                <li className="list-people-item"><Link to="/people/all-team"><b>ALL</b></Link> </li>
-                                <li className="list-people-item"><Link to="/people/my-team"><b>TEAM</b></Link> </li>
-                                <li className="list-people-item"><Link to="/people/user"><b>USER</b></Link> </li>
-                            </ul>
+                            <div className="border-header">
+                                <h1 className="card-name"><b>People</b></h1>
+                            </div>
+                            <div className="border-list">
+                                <div className="list">
+                                    <Link className="list-item" to="/people/my-team">Team</Link>
+                                    <Link className="list-item" to="/people/user">User</Link>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <Switch>
-                        <Route exact path="/people/all-team">
-                            <AllTeam/>
-                        </Route>
                         <Route path="/people/my-team">
                             <MyTeam/>
                         </Route>
