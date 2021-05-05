5093caff534496746b659b5f62d5f4318a44945b
phamtrongdong <dongptph09420@fpt.edu.vn>
2021-05-05 14:12:43 +0700

    dong

--- backend/src/carrer/carrer.service.ts
+++ backend/src/carrer/carrer.service.ts
@@ -1,18 +1,18 @@
 
-import { Injectable,NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
-import {Carrer} from './carrer.model'
+import { Carrer } from './carrer.model'
 @Injectable()
 export class CarrerService {
     constructor(@InjectModel('Carrer') private readonly carrerModel: Model<Carrer>) { }
-    
+
     //insert
     async insertCarrer(
         description: String,
         achievements: String,
         salary: Number,
-        stage:Date
+        stage: Date
     ) {
         const newCarrer = new this.carrerModel({
             description,
@@ -31,24 +31,24 @@ export class CarrerService {
     }
 
     //get a carrer by stage
-    async getSingleCarrer(id:String) {
+    async getSingleCarrer(id: String) {
         const carrer = await this.carrerModel.findById(id).exec()
         return {
             id: carrer.id,
             description: carrer.description,
             achievements: carrer.achievements,
             salary: carrer.salary,
-            stage:carrer.stage
+            stage: carrer.stage
         }
     }
 
     //update
     async updateCarrer(
-        id:String,
+        id: String,
         description: String,
         achievements: String,
         salary: Number,
-        stage:Date
+        stage: Date
     ) {
         const data = await this.carrerModel.findById(id).exec()
         if (description) {
@@ -64,12 +64,12 @@ export class CarrerService {
             data.stage = stage
         }
         data.save()
-            
+
     }
 
     //delete
     async deleteCarrer(id: String) {
-        const carrer = await this.carrerModel.findOneAndDelete({ _id: id }).exec()
+        const carrer = await this.carrerModel.deleteOne({ _id: id }).exec()
         if (carrer.n === 0) {
             throw new NotFoundException('Not found Carrer')
         }
