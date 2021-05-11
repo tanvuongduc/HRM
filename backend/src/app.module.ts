88ce81af5d0436397edf52e858a84e211612ef18
Triều Lê <ltrungtrieu@gmail.com>
2021-05-11 09:36:11 +0700

    a

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TeamModule } from './team/team.module';
-import { CompanyModule } from './company/company.module';
 import { MongooseModule } from '@nestjs/mongoose'
 import { MissionModule } from './mission/mission.module';
 import { TimeoffModule } from './timeoff/timeoff.module';
@@ -18,7 +17,6 @@ import {BASEPATH} from './base'
     }),
     UserModule,
     TeamModule,
-    CompanyModule,
     MissionModule,
     TimeoffModule],
   controllers: [AppController],
