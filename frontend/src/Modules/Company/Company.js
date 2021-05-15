9b7ddec16ed166bf41d5aedfa1ad7a65f25d403c
7un7un <dongvanhongpink@gmail.com>
2021-05-15 16:39:22 +0700

    update styled-components

--- frontend/src/Modules/Company/Company.js
+++ frontend/src/Modules/Company/Company.js
@@ -3,13 +3,14 @@ import ContactEdit from "./components/ContactEdit/ContactEdit"
 import DocumentsEdit from "./components/DocumentsEdit/DocumentsEdit"
 import OveriewEdit from "./components/OveriewEdit/OveriewEdit"
 import { Http } from "./Shared/index"
+import Div from "./Div"
 
 export default function Company() {
   const [company, setCompany] = useState({ data: [] });
 
   const addDocument = async (addDocument) => {
     try {
-      const data = await Http.post('company/documents', addDocument)
+      const data = await Http.put('company/documents', addDocument)
       setCompany(data)
     } catch (err) {
       console.log(err)
@@ -31,12 +32,12 @@ export default function Company() {
 
   const showDocument = company.data.map(e => (e.documents));
   const showContact = company.data.map(e => (e.contact));
-
+  
   return (
-    <div className="Company">
+    <Div>
       <OveriewEdit />
       <ContactEdit data={showContact} />
       <DocumentsEdit data={showDocument} addDocument={addDocument} />
-    </div>
+    </Div>
   )
 }
---
+++ frontend/src/Modules/Company/Div.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export default styled.div`
+    padding: 100px 0 50px 0;
+	  width: 100%;
+    height: auto;
+    display: flex;
+    flex-wrap: wrap;
+  `;
