9bd5b86be800e7fe20bba0b8cbf894ba99e69762
maikoniaxa <68479544+maikoniaxa@users.noreply.github.com>
2021-06-16 17:06:58 +0700

    update

--- frontend/src/Modules/Career/Components/Overview/Overview.js
+++ frontend/src/Modules/Career/Components/Overview/Overview.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Container from '@material-ui/core/Container';
-import { makeStyles } from '@material-ui/core/styles';
 import Timeline from '@material-ui/lab/Timeline';
 import TimelineItem from '@material-ui/lab/TimelineItem';
 import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
@@ -8,83 +7,62 @@ import TimelineConnector from '@material-ui/lab/TimelineConnector';
 import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
-import{ Paper,Button }from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
-import { Component } from 'react';
-import Popover from '@material-ui/core/Popover';
+import{ Paper,Button,Typography }from '@material-ui/core';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
 import FormCareer from '../../Detail/FormCareer';
-import form from '../../../../Shared/Components/Form/Form'
+import form from '../../../../Shared/Components/Form/Form';
 
-const useStyles = makeStyles((theme) => ({
-    paper: {
-        padding: '6px 16px',
-    },
-    secondaryTail: {
-        backgroundColor: theme.palette.secondary.main,
-    },
-}));
-
-class Overview extends form{
+class Overview extends Component{
     constructor(props) {
         super(props);
         this.state= {
             anchorEl:false,
-            form:this._getInitFormData({
-                name :"",
-                certNo :"",
-                reciveAt :"",
-                org :"",
-                classification :"",
-                major :"",
-                note :"",
-                degree :"",
-                docs :"",
-                status :"",
-            }),
+            open :false,
         }
     }
 
-
-
-    handleClick = (ev) => {
-        let events = ev.currentTarget
-        console.log(events);
-        this.setState({anchorEl:events})
+    handleClickOpen = () => {
+       this.setState({
+           open:true,
+       })
       };
+    
     handleClose = () => {
         this.setState({
-            anchorEl:false
+            open:false,
         })
       };
-    open = () =>{
-        Boolean(anchorEl)  
-    }
-    id =()=>{
-        this.open ? 'simple-popover' : false;
-    }
     render(){
+        let idUser ="60b9aab51f194c0c78c9932b";
+        let indexOfCert=1;
+        
         return (
             <Container maxWidth="md" style={{ paddingTop: '100px' }}>
-                  <div className="btn-addNew">
-                    <Button aria-describedby={this.id} variant="contained" color="primary" onClick={this.handleClick}>
-                        Thêm mới 
+           
+            {/* Dialog */}
+                <div>
+                    <div className ="btn-addNew">
+                    <Button variant="contained" color="primary" size="small"  onClick={this.handleClickOpen}>
+                        Thêm mới
                     </Button>
-                    <Popover
-                        open={this.state.anchorEl}
-                        anchorEl={this.state.anchorEl}
+                    </div>
+                    <Dialog
+                         className="Dialog"
+                        paper
+                        open={this.state.open}
+                        keepMounted
                         onClose={this.handleClose}
-                        anchorOrigin={{
-                        vertical: 'center',
-                        horizontal: 'center',
-                        }}
-                        transformOrigin={{
-                        vertical: 'top',
-                        horizontal: 'center',
-                        }}
+                        aria-labelledby="alert-dialog-slide-title"
+                        aria-describedby="alert-dialog-slide-description"
                     >
-                        <Typography> <FormCareer/></Typography>
-                    </Popover>
+                        <DialogContent>
+                          <FormCareer idUser={idUser} indexOfCert={indexOfCert}/>
+                        </DialogContent>
+
+                    </Dialog>
                 </div>
+                {/*  */}
                 <Timeline align="alternate">
                     <TimelineItem>
                         <TimelineOppositeContent>
--- frontend/src/Modules/Career/Detail/FormCareer.js
+++ frontend/src/Modules/Career/Detail/FormCareer.js
@@ -5,42 +5,83 @@ import {
   Button,Card,MenuItem 
 } from "@material-ui/core";
 import { FaCheck } from "react-icons/fa";
+import CareerService from "../Shared/CareerService"
+import form from '../../../Shared/Components/Form/Form';
 
-
-class FormCareer  extends Component{
+class FormCareer  extends form{
     constructor(props) {
         super(props);
         this.state = {
-           
+           certificate:[],
+           form:this._getInitFormData({
+            name :"",
+            certNo :"",
+            reciveAt :"",
+            org :"",
+            classification :"",
+            major :"",
+            note :"",
+            degree :"",
+            // docs :[],
+            status :"",
+        }),
         }
     }
+    componentDidMount = () => {
+       CareerService.getUserById(this.props.idUser).then((res)=>{
+        let certificates =res.data.certificates
+        certificates.map((data,index) => {
+            if(index == this.props.indexOfCert){
+              this.setState({certificate:data})
+              console.log(data);
+              this._fillForm({
+                name :data.name,
+                reciveAt :data.reciveAt,
+                certNo :data.certNo,
+                org :data.org,
+                classification :data.classification,
+                major :data.major,
+                note :data.note,
+                degree :data.degree,
+                // docs :data.docs,
+                status :data.status,
+              });
+            };
+        });
+     }); 
+    }
+
     render(){
+    const {idUser,indexOfCert} =this.props;
+    let form= this.state.form;
+    console.log(this.state.form);
         return(
             <Fragment>
+                <h4 className="title-career">Thông tin chi tiết</h4>
                 <Grid  className="form-career">
-                    <Grid container>
+                    <Grid container paper>
                         <Grid sm="4"  className="label" align="right">Tên chứng chỉ :</Grid>
-                        <Grid sm="8"  className="input" ><TextField fullWidth/></Grid>
+                        <Grid sm="8"  className="input" ><TextField fullWidth value={form.name.value} id="name" name ="name"  onChange={(ev) => {this._setValueNotCheck(ev,"name")}}/></Grid>
                         <Grid sm="4"  className="label" align="right">Mã số bằng :</Grid>
-                        <Grid sm="8"  className="input" ><TextField fullWidth/></Grid>
-                        <Grid sm="4"  className="label" align="right">Tổ chức xác nhận :</Grid>
-                        <Grid sm="8"  className="input" ><TextField fullWidth/></Grid>
+                        <Grid sm="8"  className="input" ><TextField fullWidth value={form.certNo.value} id="certNo" name ="certNo" onChange={(ev) => {this._setValueNotCheck(ev,"certNo")}}/></Grid>
+                        <Grid sm="4"  className="label" align="right">Tổ chức :</Grid>
+                        <Grid sm="8"  className="input" ><TextField fullWidth value={form.org.value} id="org" name ="org" onChange={(ev) => {this._setValueNotCheck(ev,"org")}}/></Grid>
                         <Grid sm="4"  className="label" align="right">Thời gian nhận  :</Grid>
-                        <Grid sm="8"  className="input" ><TextField fullWidth type="date"/></Grid>
+                        <Grid sm="8"  className="input" ><TextField fullWidth value={form.reciveAt.value} id="reciveAt" name ="reciveAt" onChange={(ev) => {this._setValueNotCheck(ev,"reciveAt")}} type="date"/></Grid>
                         <Grid sm="4"  className="label" align="right">Xếp loại :</Grid>
-                        <Grid sm="8"  className="input" ><TextField fullWidth/></Grid>
+                        <Grid sm="8"  className="input" ><TextField fullWidth  id="classification" name ="classification" onChange={(ev) => {this._setValueNotCheck(ev,"classification")}}/></Grid>
                         <Grid sm="4"  className="label" align="right">Chuyên ngành :</Grid>
-                        <Grid sm="8"  className="input" ><TextField fullWidth/></Grid>
+                        <Grid sm="8"  className="input" ><TextField fullWidth value={form.major.value} id="major" name ="major" onChange={(ev) => {this._setValueNotCheck(ev,"major")}}/></Grid>
                         <Grid sm="4"  className="label" align="right">Học vị :</Grid>
-                        <Grid sm="8"  className="input" ><TextField fullWidth/></Grid>
+                        <Grid sm="8"  className="input" ><TextField fullWidth value={form.degree.value} id="degree" name ="degree" onChange={(ev) => {this._setValueNotCheck(ev,"degree")}}/></Grid>
                         <Grid sm="4"  className="label" align="right">Đánh giá :</Grid>
-                        <Grid sm="8"  className="input" ><TextField fullWidth/></Grid>
+                        <Grid sm="8"  className="input" ><TextField fullWidth value={form.note.value} id="note" name ="note" onChange={(ev) => {this._setValueNotCheck(ev,"note")}}/></Grid>
                         <Grid sm="4"  className="label" align="right"></Grid>
-                        <Grid sm="8"  className="input" ><TextField fullWidth type="file"/></Grid>
+                        <Grid sm="8"  className="input" ><TextField fullWidth id="docs" name ="docs" onChange={(ev) => {this._setValueNotCheck(ev,"docs")}} type="file"/></Grid>
                         <Grid sm="4"  className="label" align="right">Trạng thái :</Grid>
-                        <Grid sm="8"  className="input" ><TextField>Đã duyệt<FaCheck /></TextField></Grid>
+                        <Grid sm="8"  className="label" >{form.status.value} <FaCheck /></Grid>
                         <Grid>
-                            <Button className="btn-update" variant="contained" color="primary">Cập nhật</Button>
+                            <Button className="btn-update" variant="contained" color="primary">Cập nhật </Button>
                         </Grid>
                     </Grid>
                 </Grid>
